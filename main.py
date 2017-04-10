4d21ffef0f15b38178a71af3bc69b292f7d194e4
Jacob Stoebel <jstoebel@gmail.com>
2017-04-10 13:27:18 -0400

    convert to object oriented approach

--- main.py
+++ main.py
@@ -1,83 +1,102 @@
 import argparse
 import json
+import os
 import random
 
 from jinja2 import Template
 import requests
 
-PARSER = argparse.ArgumentParser(description='Grab Codewars kata for offline use')
-PARSER.add_argument('--language', help='The language to grab from')
-PARSER.add_argument('--dest', help='The destination of the resulting markdown file')
-ARGS = PARSER.parse_args()
-SECRETS = json.load(open('secrets.json', 'r'))
+class Client:
 
-def get_kata(slug, api_key, path):
-    """
-    fetches the kata for the given slug
+    def __init__(self, args):
+        """
+        a codewars client.
+        args: command line arguments
+        """
 
-    slug(str): the name of the kata to fetch
-    api_key(str): the user's api key
-    dest(optional)(str): the relative path to where the markdown file should be written
-    """
+        self.args = args
+        self.secrets = json.load(open('secrets.json', 'r'))
 
-    if ARGS.dest is not None:
-        dest = ARGS.dest
-    else:
-        try:
-            dest = SECRETS['default_home']
-        except KeyError:
-            raise BaseException("No destination directory given and none specified in secrets.json")
+        # set destination to write kata
+        if self.args.dest is not None:
+            self.dest = self.args.dest
+        else:
+            try:
+                self.dest = self.secrets['default_home']
+            except KeyError:
+                raise BaseException("No destination directory given and none specified in secrets.json")
 
-    resp = requests.get('https://www.codewars.com/api/v1/code-challenges/{}'.format(slug))
-    resp_json = json.loads(resp.text)
+        # validate the destination
+        if not os.path.isdir(self.dest):
+            raise BaseException("destination path {} does not exsits".format(self.dest))
 
-    params = {
-        'slug': slug,
-        'name': resp_json['name'],
-        'username': resp_json['createdBy']['username'],
-        'url': resp_json['createdBy']['url'],
-        'description': resp_json['description']
-    }
-    rel_file = '{rel_path}{file_name}.md'.format(rel_path=dest, file_name=slug)
 
-    with(open(rel_file, 'w+')) as writer:
-        template = Template(open('template.md.j2', 'r').read())
-        output = template.render(**params)
-        writer.write(output)
+    def make_kata(self):
+        # set language to pull
+        default_language = self.args.language
 
-def get_slug(language, api_key):
-    """
-    determines a new kata for the user
-    returns the slug of the chosen kata
-    """
-    url = "https://www.codewars.com/api/v1/code-challenges/{}/train?strategy=random".format(language)
-    print(url)
-    headers = {'Authorization': api_key}
-    resp = requests.post(url, headers=headers)
-    resp_json = json.loads(resp.text)
-    return resp_json['slug']
+        if self.args.language is not None:
+            self.language = self.args.language
 
-def get_language(default_language, languages):
-    """
-    determines the language to pull. Draws first from CLI argument (--language)
-    and falls back to secrets.yml. If neither are provided, raises an exception.
-    """
+        else:
+            try:
+                self.language = random.choice(self.secrets['languages'])
+            except KeyError:
+                raise BaseException("No language given and none specified in secrets.json")
 
-    if default_language is not None:
-        return default_language
+        print('Finding a kata for {}'.format(self.language), end='')
+        self.get_slug()
+        self.get_kata()
 
-    else:
-        try:
-            languages = SECRETS['languages']
-            return random.choice(languages)
-        except KeyError:
-            raise BaseException("No language given and none specified in secrets.json")
+    def get_slug(self):
+        """
+        language(str): langauge to pull from
+        determines a random kata slug
+        """
+
+        url = "https://www.codewars.com/api/v1/code-challenges/{}/train?strategy=random".format(self.language)
+        headers = {'Authorization': self.secrets['api_key']}
+        resp = requests.post(url, headers=headers)
+        resp_json = json.loads(resp.text)
+        self.slug = resp_json['slug']
+
+    def get_kata(self):
+        """
+        fetches the kata for the given slug
+
+        slug(str): the name of the kata to fetch
+        """
+
+        resp = requests.get('https://www.codewars.com/api/v1/code-challenges/{}'.format(self.slug))
+        resp_json = json.loads(resp.text)
+
+        params = {
+            'slug': self.slug,
+            'name': resp_json['name'],
+            'username': resp_json['createdBy']['username'],
+            'url': resp_json['createdBy']['url'],
+            'description': resp_json['description']
+        }
+
+        rel_file = os.path.join(self.dest, '{}.md'.format(self.slug))
+
+        print(rel_file)
+        with(open(rel_file, 'w+')) as writer:
+            template = Template(open('template.md.j2', 'r').read())
+            output = template.render(**params)
+            writer.write(output)
+
+        print('-> {}'.format(rel_file))
 
 def main():
-    api_key = SECRETS['api_key']
-    language = get_language(ARGS.language, SECRETS['languages'])
-    slug = get_slug(language, api_key)
-    get_kata(slug, api_key, ARGS.dest)
+    c = Client(ARGS)
+    for _ in range(ARGS.n):
+        c.make_kata()
 
 if __name__ == '__main__':
+    PARSER = argparse.ArgumentParser(description='Grab Codewars kata for offline use')
+    PARSER.add_argument('--language', help='The language to grab from')
+    PARSER.add_argument('--dest', help='The destination of the resulting markdown file')
+    PARSER.add_argument('--n', type=int, help='Number of katas to grab')
+    ARGS = PARSER.parse_args()
     main()
