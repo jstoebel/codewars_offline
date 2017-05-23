42eb4aecc81d0c5afc4040421b7cd01f45eeb1d0
Jacob Stoebel <jstoebel@gmail.com>
2017-05-22 20:21:08 -0400

    add metadata
    add pypitest
    version bump

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup
 
-setup(name='kata_scrape',
-      version='0.1',
+setup(
+      name='kata_scrape',
+      version='0.2',
+      summary='A simple client for scraping Codewars katas',
       description='A simple client for scraping Codewars katas',
+      keywords=['codewars', 'code katas', 'selenium']
       url='https://github.com/jstoebel/kata_scrape',
       author='Jacob Stoebel',
       author_email='jstoebel@gmail.com',
+      maintainer='Jacob Stoebel'
       license='MIT',
       packages=['kata_scrape'],
       scripts=['bin/kata-scrape'],
